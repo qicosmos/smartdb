fa42f9ec6317ee0a5376c8715f31006e389c7e13
Gogs <gogs@fake.local>
2016-07-28 11:18:01 +0800

    add files.

---
+++ include/smartdb.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <string>
+#include <tuple>
+
+namespace smartdb
+{
+	class smartdb
+	{
+	public:
+		bool connect(const std::string& conn_str)
+		{
+			return false;
+		}
+
+		template<typename... Args>
+		auto excecute(const std::string& strsql, Args&&... args)
+		{
+			return 0;
+		}
+
+		template<typename Tuple, typename... Args>
+		auto excecute(const std::string& strsql, const Tuple& tp)
+		{
+			return 0;
+		}
+
+		bool prepare(const std::string& strsql)
+		{
+			return false;
+		}
+
+		//transaction: begin, end, commit, rollback
+		//todo
+
+		//range-based for
+		//todo
+
+		void close()
+		{
+
+		}
+
+	private:
+
+	};
+}
--- msvc/smartdb/main.cpp
+++ msvc/smartdb/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <define.h>
+#include <smartdb.hpp>
 
 int main()
 {
